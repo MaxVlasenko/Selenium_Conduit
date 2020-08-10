29aab0524e07f94d1cdbab0a30e8bc283cc6e349
MaxVlasenko <67264111+MaxVlasenko@users.noreply.github.com>
2020-08-10 20:52:44 +0300

    Update Register.py

--- Register.py
+++ Register.py
@@ -1,18 +1,19 @@
 from selenium import webdriver
 
-name = "mandarin1111"
-email = "gdfkkd@jdfdddjbfj.com"
+name = "mandarin122111"
+email = "gdf2kkd@jdfdddjbfj.com"
 password = "qwerngffgtyu"
 
 
 def test1():
     driver = webdriver.Chrome()
-    driver.get("https://react-redux.realworld.io/#/register?_k=0h18n4")
+    driver.get("https://react-redux.realworld.io/#/?_k=x56v6i")
+    driver.find_element_by_css_selector('[href="#register"]').click()
     driver.find_element_by_css_selector('[placeholder="Username"').send_keys(name)
     driver.find_element_by_css_selector('[placeholder="Email"').send_keys(email)
     driver.find_element_by_css_selector('[placeholder="Password"').send_keys(password)
     driver.find_element_by_css_selector('[Type="Submit"').click()
-    time.sleep(5)
+
     driver.quit()
 
 
