0029639aec5e6557081010ef0f94f8afb87e8a87
DmitriyLitvin <dimalitvin192.0@gmail.com>
2020-08-11 23:44:36 +0300

    refactored code using page object pattren

--- conduit_selenium/page.py
+++ conduit_selenium/page.py
@@ -1,6 +1,7 @@
 from time import sleep
 
-from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from faker import Faker
 from selenium import webdriver
 
 class BasePage:
@@ -10,7 +11,27 @@ class BasePage:
     def open(self, path):
         self.driver.get(path)
 
+    def check_exists_by_css_selector(self, css):
+        try:
+            self.driver.find_element_by_css_selector(css)
+        except NoSuchElementException:
+            return False
+        return True
+
+
+class TestCase(object):
+    driver = None
+
+    def setup_method(self):
+        self.driver = webdriver.Chrome(executable_path="c:/selenium/chromedriver.exe")
+        self.driver.set_window_size(1920, 1080)
+
+    def teardown_method(self):
+        self.driver.quit()
+
+
 class LoginPage(BasePage):
+    setting_selector = '[href="#settings"]'
     login_link_selector = 'a[href = \'#login\']'
     email_field_selector = '.form-group > input[type = \'email\']'
     login_field_selector = '.form-group > input[type = \'password\']'
@@ -26,12 +47,16 @@ class LoginPage(BasePage):
         login_field.send_keys(user_data['password'])
         subm_button = self.driver.find_element_by_css_selector(self.subm_button_selector);
         subm_button.click();
+        sleep(4)
+        assert self.check_exists_by_css_selector(self.setting_selector)
+
 
 class ArticlePage(BasePage):
     article_title_field_selector = 'form > fieldset > fieldset:nth-child(1) > input'
     about_article_field_selector = 'form > fieldset > fieldset:nth-child(2) > input'
     article_field_selector = 'form > fieldset > fieldset:nth-child(3) > textarea'
     tag_field_selector = 'form > fieldset > fieldset:nth-child(4) > input'
+    write_comment_field = 'form > div:nth-child(1) > textarea[placeholder="Write a comment..."]'
     publish_article_button = 'form > fieldset > button'
     article_title_field = 'GB'
     about_article_field = 'about GB'
@@ -53,6 +78,8 @@ class ArticlePage(BasePage):
         tag_field.send_keys(self.tag_field)
         publish_article_button = self.driver.find_element_by_css_selector(self.publish_article_button)
         publish_article_button.click()
+        sleep(4)
+        assert self.check_exists_by_css_selector(self.write_comment_field)
 
     def update_article(self, article_title):
         self.article_title_field = article_title
@@ -67,5 +94,59 @@ class ArticlePage(BasePage):
         assert article_title == article_title_label.text
 
 
+class SettingsPage(BasePage):
+    setting_selector = '[href="#settings"]'
+    log_out_button_selector = '.btn.btn-outline-danger'
+    short_bio_selector = '[placeholder="Short bio about you"]'
+    new_password_selector = '[placeholder="New Password"]'
+    user_name_selector = '[placeholder="Username"]'
+    submit_button_selector = '[Type="Submit"]'
+    user_name = 'Arnold Schwarz'
+    short_bio = 'Mr Olimpia'
+    sign_in_selector = 'a[href = "#login"]'
+    user_name_element = '.container > ul > li:nth-child(4) > a'
+    faker = Faker()
+
+    def log_out(self):
+        self.driver.find_element_by_css_selector(self.setting_selector).click()
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        self.driver.find_element_by_css_selector(self.log_out_button_selector).click()
+        sleep(4)
+        assert self.check_exists_by_css_selector(self.sign_in_selector)
+
+    def update_settings(self):
+        self.driver.find_element_by_css_selector(self.setting_selector).click()
+        user_name_field = self.driver.find_element_by_css_selector(self.user_name_selector)
+        user_name_field.clear()
+        user_name_field.send_keys(self.user_name)
+        short_bio_field = self.driver.find_element_by_css_selector(self.short_bio_selector)
+        short_bio_field.clear()
+        short_bio_field.send_keys(self.short_bio)
+        password_field = self.driver.find_element_by_css_selector(self.new_password_selector)
+        password_field.clear()
+        self.driver.find_element_by_css_selector(self.submit_button_selector).click()
+        sleep(4)
+        user_name_element = self.driver.find_element_by_css_selector(self.user_name_element)
+        assert user_name_element.text == self.user_name
 
 
+class RegistrationPage(BasePage):
+    sign_up_selector = 'a[href="#register"]'
+    user_name_selector = 'input[placeholder="Username"]'
+    email_selector = 'input[placeholder="Email"]'
+    password_selector = 'input[placeholder="Password"]'
+    submit_button_selector = 'button[Type="Submit"]'
+    setting_selector = '[href="#settings"]'
+    user_name = 'Dima1997'
+    user_password = 'amid1516'
+    faker = Faker()
+
+    def register_user(self):
+        self.open("https://react-redux.realworld.io/#/?_k=yawtxs")
+        self.driver.find_element_by_css_selector(self.sign_up_selector).click()
+        self.driver.find_element_by_css_selector(self.user_name_selector).send_keys(self.user_name)
+        self.driver.find_element_by_css_selector(self.email_selector).send_keys(self.faker.safe_email())
+        self.driver.find_element_by_css_selector(self.password_selector).send_keys(self.user_password)
+        self.driver.find_element_by_css_selector(self.submit_button_selector).click()
+        sleep(4)
+        assert self.check_exists_by_css_selector(self.setting_selector)
--- conduit_selenium/test.py
+++ conduit_selenium/test.py
@@ -2,34 +2,60 @@ from time import sleep
 
 import pytest
 from selenium import webdriver
-from page import LoginPage, ArticlePage
-
-USER = {
-    'login': 'kate198@gmail.com',
-    'password': 'amid1516'
-}
-
-@pytest.fixture()
-def driver():
-    driver = webdriver.Chrome(executable_path="c:/selenium/chromedriver.exe")
-    driver.set_window_size(1920, 1080)
-    yield driver
-    driver.quit()
+from page import LoginPage, ArticlePage, SettingsPage, RegistrationPage, TestCase
 
 
-def test_article(driver):
-    login_page = LoginPage(driver)
-    article_page = ArticlePage(driver)
-    login_page.login_as_registered_user(USER)
-    sleep(4)
-    new_post_link = driver.find_element_by_css_selector('div[class = \'container\'] > ul > li:nth-child(2) > a');
-    new_post_link.click()
-    sleep(4)
-    article_page.create_article()
-    sleep(4)
-    edit_button = driver.find_element_by_css_selector('.banner > div > div > span > a')
-    edit_button.click()
-    sleep(4)
-    article_page.update_article('Great Britain')
+class TestConduit(TestCase):
+    USER = {
+        'login': 'kate198@gmail.com',
+        'password': 'amid1987'
+    }
 
+    def test_create_article(self):
+        login_page = LoginPage(self.driver)
+        article_page = ArticlePage(self.driver)
+        login_page.login_as_registered_user(self.USER)
+        sleep(4)
+        new_post_link = self.driver.find_element_by_css_selector(
+            'div[class = \'container\'] > ul > li:nth-child(2) > a');
+        new_post_link.click()
+        sleep(4)
+        article_page.create_article()
 
+    def test_update_article(self):
+        login_page = LoginPage(self.driver)
+        article_page = ArticlePage(self.driver)
+        login_page.login_as_registered_user(self.USER)
+        sleep(4)
+        new_post_link = self.driver.find_element_by_css_selector(
+            'div[class = \'container\'] > ul > li:nth-child(2) > a');
+        new_post_link.click()
+        sleep(4)
+        article_page.create_article()
+        sleep(4)
+        edit_button = self.driver.find_element_by_css_selector('.banner > div > div > span > a')
+        edit_button.click()
+        sleep(4)
+        article_page.update_article('Great Britain')
+
+    def test_log_out(self):
+        login_page = LoginPage(self.driver)
+        login_page.login_as_registered_user(self.USER)
+        sleep(4)
+        settings_page = SettingsPage(self.driver)
+        settings_page.log_out()
+
+    def test_update_settings(self):
+        login_page = LoginPage(self.driver)
+        login_page.login_as_registered_user(self.USER)
+        sleep(4)
+        settings_page = SettingsPage(self.driver)
+        settings_page.update_settings()
+
+    def test_login(self):
+        login_page = LoginPage(self.driver)
+        login_page.login_as_registered_user(self.USER)
+
+    def test_register(self):
+        register_page = RegistrationPage(self.driver)
+        register_page.register_user()
